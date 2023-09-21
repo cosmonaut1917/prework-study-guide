88020b19fcfb69873731eb7bd0caddd96c75cb6a
james <jamesdwire66@gmail.com>
2023-09-20 18:21:38 -0600

    added javascript to the project

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++){
+console.log (topics[x]);
+}
+/*
+if (topic === 'HTML'){
+    console.log("let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log ("let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("let's study Git!");
+} else if (topic === 'Javascript'){
+    console.log("let's study Java script!");
+} else {
+    console.log("please try again!")
+
+}
+*/
